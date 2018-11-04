5233417ae167d431ba0365d19eb2269bea95e9c0
Ánh Kevin <anh_kevin@hotmail.com>
2018-11-04 19:24:11 +0700

    Update get_last_post_news.php

--- get_last_post_news.php
+++ get_last_post_news.php
@@ -9,8 +9,22 @@
 <?php if ( $the_query->have_posts() ) : ?>
   <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-    <?php the_title(); ?>
-    <?php the_excerpt(); ?>
+      // Title
+     <?php the_title(); ?>
+     <?php the_excerpt(); ?>
+
+      // link
+     <?php echo get_permalink( $post->ID );?>
+
+      // Custom field
+     <?= get_post_meta(get_the_ID(),'label_description',true);?>
+
+      // image
+     <?php if (has_post_thumbnail( $post->ID ) ): ?>
+     <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+     <img src="<?php echo $image[0]; ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+     <?php endif; ?>
+
 
   <?php endwhile; ?>
   <?php wp_reset_postdata(); ?>
